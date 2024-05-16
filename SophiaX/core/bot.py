ff0c769924cc85fc55f8efaa55b04ccbf8a6e447
Dark Devil <123370768+darkdevil-dev@users.noreply.github.com>
2024-05-16 11:51:55 +0530

    Update bot.py

--- SophiaX/core/bot.py
+++ SophiaX/core/bot.py
@@ -6,7 +6,7 @@ import config
 from ..logging import LOGGER
 
 
-class Slayer(Client):
+class Sophia(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
